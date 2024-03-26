687b4196f698947dd38a377f186c19bb7aabd8a8
snehpnp <snehpnp@gmail.com>
2024-03-26 18:07:00 +0530

    dopmneewjbsdh

--- Frontend/src/Layouts/Admin/Sub/Sub.js
+++ Frontend/src/Layouts/Admin/Sub/Sub.js
@@ -40,7 +40,7 @@ const Sub = () => {
             expiringOn: '15 Feb 2024',
             status: 'Paid', // Changed to text
             invoice: <a href="invoice-subscription.html" className="invoice-detail"><i className="fe fe-file-text" /></a> // Changed to static icon
-        },   {
+        }, {
             id: 1,
             subscriber: 'Hermann Groups',
             plan: 'Advanced (Monthly)',
@@ -93,12 +93,110 @@ const Sub = () => {
         </div>
     );
 
+const HeaderAndFilter = (
+    <div className="page-header">
+    <div className="content-page-header">
+        <h5>Subscriber List</h5>
+        <div className="page-content">
+            <div className="list-btn">
+                <ul className="filter-list">
+                    <li>
+                        <a
+                            className="btn-filters"
+                            href="javascript:void(0);"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="bottom"
+                            title="Refresh"
+                        >
+                            <span>
+                                <i className="fe fe-refresh-ccw" />
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="btn btn-filters w-auto popup-toggle"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="bottom"
+                            title="Filter"
+                        >
+                            <span className="me-2">
+                                <img src="assets/img/icons/filter-icon.svg" alt="filter" />
+                            </span>
+                            Filter
+                        </a>
+                    </li>
+                    <li>
+                        <div
+                            className="dropdown dropdown-action"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="bottom"
+                            title="Download"
+                        >
+                            <a
+                                href="#"
+                                className="btn btn-filters"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <span className="me-2">
+                                    <i className="fe fe-download" />
+                                </span>
+                                Export
+                            </a>
+                            <div className="dropdown-menu dropdown-menu-end">
+                                <ul className="d-block">
+                                    <li>
+                                        <a
+                                            className="d-flex align-items-center download-item"
+                                            href="javascript:void(0);"
+                                            download=""
+                                        >
+                                            <i className="far fa-file-pdf me-2" />
+                                            Export as PDF
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="d-flex align-items-center download-item"
+                                            href="javascript:void(0);"
+                                            download=""
+                                        >
+                                            <i className="far fa-file-text me-2" />
+                                            Export as Excel
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a
+                            className="btn btn-filters"
+                            href="javascript:void(0);"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="bottom"
+                            title="Print"
+                        >
+                            <span className="me-2">
+                                <i className="fe fe-printer" />
+                            </span>{" "}
+                            Print
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+)
+
     return (
         <div>
             <div className="content container-fluid">
 
-   {/* PAGE HEADER */}
-   <div className="page-header">
+                {/* PAGE HEADER */}
+                <div className="page-header">
                     <div className="content-page-header">
                         <h5>Subscriber List</h5>
                         <div className="page-content">
@@ -279,14 +377,14 @@ const Sub = () => {
                                             </thead>
                                             <tbody>
                                                 {/* Dynamically render data rows */}
-                                                {data.map((row,index) => (
+                                                {data.map((row, index) => (
                                                     <tr key={row.id}>
                                                         {columns.map(column => (
                                                             <td key={column.key}>
                                                                 {column.key === 'action' ? actionButton :
-                                                                    column.key === 'status' ? <span className={`badge ${row[column.key] === 'Paid' ? 'bg-success' : 'bg-danger'} d-inline-flex align-items-center`}>{row[column.key]}</span> : 
-                                                                        column.key === 'invoice' ? row[column.key] : 
-                                                                            row[column.key]} 
+                                                                    column.key === 'status' ? <span className={`badge ${row[column.key] === 'Paid' ? 'bg-success' : 'bg-danger'} d-inline-flex align-items-center`}>{row[column.key]}</span> :
+                                                                        column.key === 'invoice' ? row[column.key] :
+                                                                            row[column.key]}
                                                             </td>
                                                         ))}
                                                     </tr>
