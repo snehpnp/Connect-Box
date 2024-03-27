01c330fda7317a95a4b27cf914166b9032b23be9
snehpnp <snehpnp@gmail.com>
2024-03-27 11:57:59 +0530

    done

--- Frontend/src/Routes/Admin_routes.jsx
+++ Frontend/src/Routes/Admin_routes.jsx
@@ -1,4 +1,4 @@
-import React, { Profiler } from 'react';
+import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import Overview from '../Layouts/Admin/Dashboard/Overview';
