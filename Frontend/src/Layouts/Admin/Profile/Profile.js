e691bef0d7bc85b77ebb5df9dfcf3ab0f4dd2218
snehpnp <snehpnp@gmail.com>
2024-03-29 11:45:40 +0530

    sss

--- Frontend/src/Layouts/Admin/Profile/Profile.js
+++ Frontend/src/Layouts/Admin/Profile/Profile.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ProfileApi from '../ProfileApi/ProfileApi'
 
 const Profile = () => {
     const [profileData, setProfileData] = useState([]);
