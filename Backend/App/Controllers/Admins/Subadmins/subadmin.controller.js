5c8732fa09360165efdd756b116665b4b4a3f1d3
snehpnp <snehpnp@gmail.com>
2024-04-03 13:48:24 +0530

    done

--- Backend/App/Controllers/Admins/Subadmins/subadmin.controller.js
+++ Backend/App/Controllers/Admins/Subadmins/subadmin.controller.js
@@ -287,6 +287,8 @@ class Subadmin {
       console.log("Error getallSubadmin error -", error);
     }
   }
+
+
   async GetAllRechargeDetails(req, res) {
     try {
       const { Role } = req.body;
@@ -336,6 +338,45 @@ class Subadmin {
       res.send({ status: false, msg: "Internal Server Error" });
     }
   }
+
+
+  async UpdateActiveStatusSubadmin(req, res) {
+    try {
+      const { id, user_active_status } = req.body;
+      // UPDATE ACTTIVE STATUS CLIENT
+
+      const get_user = await User_model.find({ _id: id });
+      if (get_user.length == 0) {
+        return res.send({
+          status: false,
+          msg: "Empty data",
+          data: []
+        });
+      }
+
+      const filter = { _id: id };
+      const updateOperation = { $set: { ActiveStatus: user_active_status } };
+
+      const result = await User_model.updateOne(filter, updateOperation);
+
+      if (result) {
+        // STATUS UPDATE SUCCESSFULLY
+        var status_msg = user_active_status == "0" ? "DeActivate" : "Activate";
+      
+
+        res.send({
+          status: true,
+          msg: "Update Successfully",
+          data: result,
+        });
+      }
+    } catch (error) {
+      console.log("Error trading status Error-", error);
+    }
+  }
+
+
+
 }
 
 module.exports = new Subadmin();
--- Backend/App/Routes/Admin/Subadmin.routes.js
+++ Backend/App/Routes/Admin/Subadmin.routes.js
@@ -4,7 +4,7 @@
 const router = require("express").Router()
 const {verifyToken}= require('../../Middlewares/autt.middleware')
 
-const { AddSubadmin,EditSubadmin,getallSubadmin,getallSubadminClients,getOneSubadmin,GetAllRechargeDetails} = require('../../Controllers/Admins/Subadmins/subadmin.controller')
+const { AddSubadmin,EditSubadmin,getallSubadmin,getallSubadminClients,getOneSubadmin,GetAllRechargeDetails,UpdateActiveStatusSubadmin} = require('../../Controllers/Admins/Subadmins/subadmin.controller')
 
 
 
@@ -16,6 +16,9 @@ router.post('/subadmin/clients/getall', getallSubadminClients);
 router.post('/subadmin/get', getOneSubadmin);
 
 router.post('/recharge/get', GetAllRechargeDetails);
+
+router.post('/activestatus/update', UpdateActiveStatusSubadmin);
+
 
 
 
