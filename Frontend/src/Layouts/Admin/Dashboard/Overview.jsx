be892f9b21c4fcae8fcacd0ed5cdba105c09df2f
snehpnp <snehpnp@gmail.com>
2024-04-06 15:36:49 +0530

    ddd

--- Frontend/src/Layouts/Admin/Dashboard/Overview.jsx
+++ Frontend/src/Layouts/Admin/Dashboard/Overview.jsx
@@ -30,12 +30,11 @@ const Overview = () => {
   const [adminData, setAdminData] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const maxPercentage = 10;
+  const maxPercentage = adminData.Totalcount;
   const calculatePercentage = (count) =>
     count !== undefined && count !== null
       ? (count / maxPercentage) * 100
       : null;
-
   const percentages = {
     percentage: calculatePercentage(adminData.Totalcount),
     percentage1: calculatePercentage(adminData.TotalActivecount),
@@ -64,10 +63,10 @@ const Overview = () => {
         if (response.status) {
           toast.success(response.msg);
           setAdminData(response.data);
-      
+
           const categories = response.data.dummyData.categories;
           const data = response.data.dummyData.data;
-      
+
           setOptions(prevOptions => ({
             ...prevOptions,
             xaxis: {
@@ -75,7 +74,7 @@ const Overview = () => {
               categories: categories
             }
           }));
-      
+
           setSeries([{ name: "series-1", data: data }]);
         } else {
           toast.error(response.msg);
@@ -84,7 +83,7 @@ const Overview = () => {
       .catch((error) => {
         console.log("Error", error);
       });
-      
+
   };
 
   const cardsData = [
@@ -103,7 +102,7 @@ const Overview = () => {
           ? "fas fa-arrow-down"
           : "fas fa-arrow-up",
       percentageChange:
-        percentage !== null ? `${Math.round(percentage / 10) * 10}%` : "N/A",
+        percentage !== null ? `${Math.round(percentage)}%` : "N/A",
       sinceLastWeek: "since last week",
       progressBarClass:
         percentage !== null && percentage < 100 ? "bg-5" : "bg-6",
@@ -123,7 +122,7 @@ const Overview = () => {
           ? "fas fa-arrow-down"
           : "fas fa-arrow-up",
       percentageChange:
-        percentage1 !== null ? `${Math.round(percentage1 / 10) * 10}%` : "N/A",
+        percentage1 !== null ? `${Math.round(percentage1)}%` : "N/A",
       sinceLastWeek: "since last week",
       progressBarClass:
         percentage1 !== null && percentage1 < 100 ? "bg-5" : "bg-6",
@@ -143,7 +142,7 @@ const Overview = () => {
           ? "fas fa-arrow-down"
           : "fas fa-arrow-up",
       percentageChange:
-        percentage3 !== null ? `${Math.round(percentage3 / 10) * 10}%` : "N/A",
+        percentage3 !== null ? `${Math.round(percentage3)}%` : "N/A",
       sinceLastWeek: "since last week",
       progressBarClass:
         percentage3 !== null && percentage3 < 100 ? "bg-5" : "bg-6",
@@ -163,7 +162,7 @@ const Overview = () => {
           ? "fas fa-arrow-down"
           : "fas fa-arrow-up",
       percentageChange:
-        percentage4 !== null ? `${Math.round(percentage4 / 10) * 10}%` : "N/A",
+        percentage4 !== null ? `${Math.round(percentage4)}%` : "N/A",
       sinceLastWeek: "since last week",
       progressBarClass:
         percentage4 !== null && percentage4 < 100 ? "bg-5" : "bg-6",
@@ -183,7 +182,7 @@ const Overview = () => {
           ? "fas fa-arrow-down"
           : "fas fa-arrow-up",
       percentageChange:
-        percentage5 !== null ? `${Math.round(percentage5 / 10) * 10}%` : "N/A",
+        percentage5 !== null ? `${Math.round(percentage5)}%` : "N/A",
       sinceLastWeek: "since last week",
       progressBarClass:
         percentage5 !== null && percentage5 < 100 ? "bg-5" : "bg-6",
@@ -203,7 +202,7 @@ const Overview = () => {
           ? "fas fa-arrow-down"
           : "fas fa-arrow-up",
       percentageChange:
-        percentage6 !== null ? `${Math.round(percentage6 / 10) * 10}%` : "N/A",
+        percentage6 !== null ? `${Math.round(percentage6)}%` : "N/A",
       sinceLastWeek: "since last week",
       progressBarClass:
         percentage6 !== null && percentage6 < 100 ? "bg-5" : "bg-6",
@@ -279,144 +278,149 @@ const Overview = () => {
 
 
           <div className="row" data-aos="fade-left">
-            <div className="col-xl-6 d-flex">
-              <div className="card flex-fill">
-                <div className="card-header">
-                  <div className="d-flex justify-content-between align-items-center">
-                    <h5 className="card-title">Invoice Analytics</h5>
-                    <div className="dropdown main">
-                      <button
-                        className="btn btn-white btn-sm dropdown-toggle"
-                        type="button"
-                        id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        Monthly
-                      </button>
-                      <ul
-                        className="dropdown-menu"
-                        aria-labelledby="dropdownMenuButton1"
-                      >
-                        <li>
-                          <a className="dropdown-item">Monthly</a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item">Quaterly</a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item">Half Yearly</a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item">Yearly</a>
-                        </li>
-                      </ul>
+
+            <div className="row" data-aos="fade-left">
+              <div className="col-xl-6 d-flex">
+                <div className="card flex-fill">
+                  <div className="card-header">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <h5 className="card-title">Invoice Analytics</h5>
+                      <div className="dropdown main">
+                        <button
+                          className="btn btn-white btn-sm dropdown-toggle"
+                          type="button"
+                          id="dropdownMenuButton1"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          Monthly
+                        </button>
+                        <ul
+                          className="dropdown-menu"
+                          aria-labelledby="dropdownMenuButton1"
+                        >
+                          <li>
+                            <a className="dropdown-item">Monthly</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item">Quaterly</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item">Half Yearly</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item">Yearly</a>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="card-body">
-                  <div id="invoice_chart" />
-                  <div className="text-center text-muted">
-                    <div className="row">
-                      <div className="mixed-chart">
-                        <Chart
-                          colors={colors}
+                  <div className="card-body">
+                    <div id="invoice_chart" />
+                    <div className="text-center text-muted">
+                      <div className="row">
+                        <div className="mixed-chart">
+                          <Chart
+                            colors={colors}
 
-                          options={options}
-                          series={series}
-                          type="bar"
-                          width="100%"
+                            options={options}
+                            series={series}
+                            type="bar"
+                            width="100%"
 
-                        />
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="col-xl-6 d-flex">
-              <div className="card flex-fill">
-                <div className="card-header">
-                  <div className="d-flex justify-content-between align-items-center">
-                    <h5 className="card-title">Invoice 1 Analytics</h5>
-                    <div className="dropdown main">
-                      <button
-                        className="btn btn-white btn-sm dropdown-toggle"
-                        type="button"
-                        id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        Monthly
-                      </button>
-                      <ul
-                        className="dropdown-menu"
-                        aria-labelledby="dropdownMenuButton1"
-                      >
-                        <li>
-                          <a className="dropdown-item">Weekly</a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item">Monthly</a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item">Yearly</a>
-                        </li>
-                      </ul>
+              <div className="col-xl-6 d-flex">
+                <div className="card flex-fill">
+                  <div className="card-header">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <h5 className="card-title">Invoice 1 Analytics</h5>
+                      <div className="dropdown main">
+                        <button
+                          className="btn btn-white btn-sm dropdown-toggle"
+                          type="button"
+                          id="dropdownMenuButton1"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          Monthly
+                        </button>
+                        <ul
+                          className="dropdown-menu"
+                          aria-labelledby="dropdownMenuButton1"
+                        >
+                          <li>
+                            <a className="dropdown-item">Weekly</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item">Monthly</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item">Yearly</a>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="card-body">
-                  <div id="invoice_chart" />
-                  <div className="text-center text-muted">
-                    <div className="row">
-                      <div className="col-4">
-                        <div className="mt-4">
-                          <p className="mb-2 text-truncate">
-                            <i className="fas fa-circle text-primary me-1" />{" "}
-                            Invoiced
-                          </p>
-                          <h5>$2,132</h5>
+                  <div className="card-body">
+                    <div id="invoice_chart" />
+                    <div className="text-center text-muted">
+                      <div className="row">
+                        <div className="col-4">
+                          <div className="mt-4">
+                            <p className="mb-2 text-truncate">
+                              <i className="fas fa-circle text-primary me-1" />{" "}
+                              Invoiced
+                            </p>
+                            <h5>$2,132</h5>
+                          </div>
+                        </div>
+                        <div className="col-4">
+                          <div className="mt-4">
+                            <p className="mb-2 text-truncate">
+                              <i className="fas fa-circle text-success me-1" />{" "}
+                              Received
+                            </p>
+                            <h5>$1,763</h5>
+                          </div>
+                        </div>
+                        <div className="col-4">
+                          <div className="mt-4">
+                            <p className="mb-2 text-truncate">
+                              <i className="fas fa-circle text-danger me-1" />{" "}
+                              Pending
+                            </p>
+                            <h5>$973</h5>
+                          </div>
                         </div>
                       </div>
-                      <div className="col-4">
-                        <div className="mt-4">
-                          <p className="mb-2 text-truncate">
-                            <i className="fas fa-circle text-success me-1" />{" "}
-                            Received
-                          </p>
-                          <h5>$1,763</h5>
-                        </div>
+                      <div
+                        className="gif-div"
+                        style={{ height: "400px", marginTop: "-60px" }}
+                      >
+                        <iframe src="https://lottie.host/embed/703aa556-aee8-45e4-a279-c6b636b0542f/rTWOHxoaxl.json"></iframe>
                       </div>
-                      <div className="col-4">
-                        <div className="mt-4">
-                          <p className="mb-2 text-truncate">
-                            <i className="fas fa-circle text-danger me-1" />{" "}
-                            Pending
-                          </p>
-                          <h5>$973</h5>
-                        </div>
-                      </div>
-                    </div>
-                    <div
-                      className="gif-div"
-                      style={{ height: "400px", marginTop: "-60px" }}
-                    >
-                      <iframe src="https://lottie.host/embed/703aa556-aee8-45e4-a279-c6b636b0542f/rTWOHxoaxl.json"></iframe>
-                    </div>
 
+                    </div>
                   </div>
                 </div>
               </div>
+
+
             </div>
-
-
           </div>
+
+
         </div>
       </div>
-    </div>
-  );
+      </div>
+      );
 };
 
-export default Overview;
+      export default Overview;
