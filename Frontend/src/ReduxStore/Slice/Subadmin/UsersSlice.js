49c2bb5f5ffc6804e0f17455d54fa458421f473d
GanpatKoli <75944808+ganpatkoli@users.noreply.github.com>
2024-04-08 15:52:54 +0530

    done

--- Frontend/src/ReduxStore/Slice/Subadmin/UsersSlice.js
+++ Frontend/src/ReduxStore/Slice/Subadmin/UsersSlice.js
@@ -1,19 +1,43 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { GET_ALL_USERS} from "../../../Services/Subadmin/all.service";
+import { GET_ALL_USERS, ADD_USERS ,GET_ALL_BROKER } from "../../../Services/Subadmin/all.service";
 
 
 export const GetAllUsers = createAsyncThunk("user/getAll",
     async (data) => {
-        
+
         try {
             const res = await GET_ALL_USERS(data);
-            
+
             return res;
         } catch (err) {
             throw err;
         }
     }
 );
+export const AddUsers = createAsyncThunk("user/add",
+    async (data) => {
+
+        try {
+            const res = await ADD_USERS(data);
+
+            return res;
+        } catch (err) {
+            throw err;
+        }
+    }
+);
+
+export const Get_All_Broker = createAsyncThunk("broker/get",
+    async () => {
+        try {
+            const res = await GET_ALL_BROKER();
+            console.log("res :", res)
+            return res;
+        }
+        catch (err) {
+            throw err;
+        }
+    });
 
 
 
@@ -23,7 +47,9 @@ const StrategySlice = createSlice({
         isLoading: false,
         isError: false,
         userslice: null,
-      
+        add_users: null,
+        get_all_broker:null,
+
 
     },
     reducers: {},
@@ -44,10 +70,20 @@ const StrategySlice = createSlice({
 
                 state.isLoading = false;
                 state.isError = true;
-            })  
+            })
+            .addCase(AddUsers.fulfilled, (state, action) => {
 
-          
-         
+                state.isLoading = false;
+                state.add_users = action.payload;
+            })
+            .addCase(Get_All_Broker.fulfilled, (state, action) => {
+
+                state.isLoading = false;
+                state.get_all_broker = action.payload;
+            })
+
+
+
     },
 });
 
