5f5968fd7abf9e437357a6cb5294ebfb16592f9c
snehpnp <snehpnp@gmail.com>
2024-04-25 16:03:00 +0530

    eewewe

--- Frontend/src/Routes/User.routes.js
+++ Frontend/src/Routes/User.routes.js
@@ -5,18 +5,22 @@ import Header from '../Components/Dashboard/Header/Header';
 import MainHeader from '../Components/Dashboard/Header/Main_header';
 import Faqs from '../Layouts/Comman/Faqs';
 import Dashboards from '../Layouts/User/Dashboard/Dashboards';
-
 import Help from '../Layouts/Comman/Help/Helpuser';
 import Stocklist from '../Layouts/User/Stock/Clientservice';
 import Strategies from '../Layouts/User/Strategy/Strategies';
-
-
 import Profile from '../Layouts/Comman/Profile/Profile';
 import Bresponse from '../Layouts/User/Brokerresponse/Bresponse';
 
+import Orders from '../Layouts/User/Trade/Order';
+import TradeHistory from '../Layouts/User/Trade/TradeHistory';
+
+import Subscriptiondata from '../Layouts/User/Subscription/Subscriptiondata';
+
+
+
+
 
 // strategydesc
-
 const User_Routing = () => {
 
 
@@ -34,9 +38,15 @@ const User_Routing = () => {
                     <Route path="/help" element={<Help />} />
                     <Route path="/stock" element={<Stocklist />} />
                     <Route path="/strategy" element={<Strategies />} />
-                    <Route path="/broker" element={<Bresponse />} />
+                    <Route path="/broker-response" element={<Bresponse />} />
 
                     <Route exact path="/profile" element={<Profile />} />
+                    <Route exact path="/orders" element={<Orders />} />
+                    <Route exact path="/positions" element={<TradeHistory />} />
+                    <Route exact path="/subscription" element={<Subscriptiondata />} />
+
+
+
 
 
 
