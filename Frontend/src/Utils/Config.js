f137c3f207f89c3239b840ffeeb996cb59cf5d7b
snehpnp <snehpnp@gmail.com>
2024-04-29 09:19:40 +0530

    aaa

--- Frontend/src/Utils/Config.js
+++ Frontend/src/Utils/Config.js
@@ -3,8 +3,8 @@
 
 export const react_domain = `${window.location.origin}`;
 
-export const base_url = `http://185.209.75.6:7700/`;
-// export const base_url = `http://localhost:7000/`;
+// export const base_url = `http://185.209.75.6:7700/`;
+export const base_url = `http://localhost:7000/`;
 
 
 export const broker_redirect_url = "http://185.209.75.6:7700/";
