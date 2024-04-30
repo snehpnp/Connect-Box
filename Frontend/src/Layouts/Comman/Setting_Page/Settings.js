67026d00884cfd239888b2f4ebe0b3bc7063125a
himanshukubde <himanshukd25@gmail.com>
2024-04-30 12:54:01 +0530

    done

--- Frontend/src/Layouts/Comman/Setting_Page/Settings.js
+++ Frontend/src/Layouts/Comman/Setting_Page/Settings.js
@@ -1370,21 +1370,14 @@ const Settings = () => {
                                                 </div>
                                             </div>
                                         </div>
-
                                         <Apicreate_info />
                                     </div>
-
-
-
-
-
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
