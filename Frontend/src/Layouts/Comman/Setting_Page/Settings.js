639d3aab3db859b82149bd4cd725ae7d72fdf3a2
himanshukubde <himanshukd25@gmail.com>
2024-04-30 12:11:01 +0530

    done

--- Frontend/src/Layouts/Comman/Setting_Page/Settings.js
+++ Frontend/src/Layouts/Comman/Setting_Page/Settings.js
@@ -1298,17 +1298,50 @@ const Settings = () => {
                                     >
                                         <div className="col-xl-12 col-md-12">
                                             <div className="page-header">
-                                                <div className="content-page-header">
+                                                {/* <div className="content-page-header">
                                                     <h5>Change Password</h5>
-                                                    <div className="page-content">
-                                                        <div className="list-btn">
-                                                            <ul className="filter-list">
-                                                                <li>
 
-                                                                </li>
-                                                            </ul>
+                                                </div> */}
+
+                                                <div className="page-content">
+                                                    <div className="mainDiv">
+                                                        <div className="cardStyle">
+                                                            <form action="" method="post" name="signupForm" id="signupForm">
+
+                                                                <h5 className="formTitle">Change Password</h5>
+                                                                <div className="inputDiv">
+                                                                    <label className="inputLabel" htmlFor="password">
+                                                                        Current Password
+                                                                    </label>
+                                                                    <input type="password" id="password" name="password" required="" />
+                                                                </div>
+                                                                <div className="inputDiv">
+                                                                    <label className="inputLabel" htmlFor="password">
+                                                                        New Password
+                                                                    </label>
+                                                                    <input type="password" id="password" name="password" required="" />
+                                                                </div>
+                                                                <div className="inputDiv">
+                                                                    <label className="inputLabel" htmlFor="confirmPassword">
+                                                                        Confirm New Password
+                                                                    </label>
+                                                                    <input type="password" id="confirmPassword" name="confirmPassword" />
+                                                                </div>
+                                                                <div className="buttonWrapper">
+                                                                    <button
+                                                                        type="submit"
+                                                                        id="submitButton"
+
+                                                                        className="submitButton"
+                                                                    >
+                                                                        <span>Submit</span>
+
+                                                                    </button>
+                                                                </div>
+                                                            </form>
                                                         </div>
                                                     </div>
+
                                                 </div>
                                             </div>
 
