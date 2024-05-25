ffd46aeb06f490741f04efc5657c07a594fdd7a8
snehpnp <snehpnp@gmail.com>
2024-05-25 15:24:24 +0530

    d

--- Frontend/src/Utils/Config.js
+++ Frontend/src/Utils/Config.js
@@ -8,8 +8,8 @@ export const socket_Url = `https://connectbox.tradestreet.in:1001`;
 
 
 
-export const base_url = `http://localhost:7700/`;
-// export const base_url = `https://connectbox.tradestreet.in/backend/`;
+// export const base_url = `http://localhost:7700/`;
+export const base_url = `https://connectbox.tradestreet.in/backend/`;
 
 
 export const broker_backend = `https://connectbox.tradestreet.in/signal/broker-signals`;
