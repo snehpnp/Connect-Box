c621bbacf403f65d0522abc10bf20093d19e484f
snehpnp <snehpnp@gmail.com>
2024-10-05 10:57:55 +0530

    dsdsd

--- Frontend/src/Utils/Config.js
+++ Frontend/src/Utils/Config.js
@@ -1,17 +1,17 @@
 export const react_domain = `${window.location.origin}`;
 
 // // LOCAL PANEL
-export const broker_backend = `http:/localhost:8000/signal/broker-signals`;
-export const base_url = `http://localhost:7700/`;
-export const broker_redirect_url = "https://connectbox.tradestreet.in/backend/";
-export const socket_Url = `https://connectbox.tradestreet.in:1001`;
+// export const broker_backend = `http:/localhost:8000/signal/broker-signals`;
+// export const base_url = `http://localhost:7700/`;
+// export const broker_redirect_url = "https://connectbox.tradestreet.in/backend/";
+// export const socket_Url = `https://connectbox.tradestreet.in:1001`;
 
 
 // // CONNECT BOX LIVE PANEL
-// export const broker_redirect_url = "https://connectbox.tradestreet.in/backend/";
-// export const socket_Url = `https://connectbox.tradestreet.in:1001`;
-// export const broker_backend = `https://connectbox.tradestreet.in/signal/broker-signals`;
-// export const base_url = `https://connectbox.tradestreet.in/backend/`;
+export const broker_redirect_url = "https://connectbox.tradestreet.in/backend/";
+export const socket_Url = `https://connectbox.tradestreet.in:1001`;
+export const broker_backend = `https://connectbox.tradestreet.in/signal/broker-signals`;
+export const base_url = `https://connectbox.tradestreet.in/backend/`;
 
 
 // THE ALGO CONNECTBOX LIVE
