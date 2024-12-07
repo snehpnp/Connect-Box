e9af17889b0db3a6181c28fda73d677b5b77daa6
SHKGIT007 <shkgit007@gmail.com>
2024-12-07 15:57:03 +0530

    Done

--- Frontend/src/Utils/Config.js
+++ Frontend/src/Utils/Config.js
@@ -1,5 +1,11 @@
 export const react_domain = `${window.location.origin}`;
 
+export const broker_redirect_url = `${window.location.origin}/backend/`
+export const base_url = `http://localhost:7701/`;
+export const broker_backend = `${window.location.origin}/signal/broker-signals`;
+
+
+
 // // LOCAL PANEL
 // export const broker_backend = `http:/localhost:8000/signal/broker-signals`;
 // export const base_url = `http://localhost:7700/`;
@@ -8,10 +14,10 @@ export const react_domain = `${window.location.origin}`;
 
 
 // CONNECT BOX LIVE PANEL
-export const broker_redirect_url = "https://connectbox.tradestreet.in/backend/";
-export const socket_Url = `https://connectbox.tradestreet.in:1001`;
-export const broker_backend = `https://connectbox.tradestreet.in/signal/broker-signals`;
-export const base_url = `https://connectbox.tradestreet.in/backend/`;
+// export const broker_redirect_url = "https://connectbox.tradestreet.in/backend/";
+// export const socket_Url = `https://connectbox.tradestreet.in:1001`;
+// export const broker_backend = `https://connectbox.tradestreet.in/signal/broker-signals`;
+// export const base_url = `https://connectbox.tradestreet.in/backend/`;
 
 
 // THE ALGO CONNECTBOX LIVE
